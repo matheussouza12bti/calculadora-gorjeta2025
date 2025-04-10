b197f8b83ed388a3ff75a28f3678a316c47c304d
math.souzatrabalhos <math.souzatrabalhos@gmail.com>
2025-04-10 00:06:14 +0000

    inicio da logica

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-et bill = 0
-  let tipPorcentage = 0
+let bill = 0
+  let tipPercentage = 0
   let numberOfPeople = 0
+  let buttonSelected = null
  
      function reciveBillValue(){
      bill = document.querySelector("#bill").valueAsNumber
@@ -10,3 +11,8 @@ et bill = 0
      function receiveOfpeopleValue(){
        numberOfPeople = document.querySelector("#people").valueAsNumber
      }
+     function receiveTipPercentageValue(value) {
+        tipPercentage = value / 100
+        buttonSelected = document.querySelector(`#button-${value}`)
+        buttonSelected.classList.add("button-selected")
+        }
